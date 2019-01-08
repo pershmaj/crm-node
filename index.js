d8a26a7dc46c6de699a2ce08ac43a85f9e76a282
pershmaj <averingd@gmail.com>
2019-01-08 18:30:53 +0400

    added mail sender

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var sendpulse = require('sendpulse-api')
 var API_USER_ID = require('./db').API_USER_ID
 var API_SECRET = require('./db').API_SECRET
 var TOKEN_STORAGE = require('./db').TOKEN_STORAGE
+var SENDER_EMAIL = require('./db').SENDER_EMAIL
 
 
 const { APP_PORT, APP_IP, APP_PATH } = process.env;
@@ -63,6 +64,7 @@ io.on('connection', function (socket) {
 
     socket.on('delete', (msg) => {
         db.collection(msg.ent).findOneAndDelete({_id: ObjectId(msg.data)}).then((result) => {
+            console.log(result)
             io.emit('delete', {ent: msg.ent, data: msg.data})
         }).catch((err) => console.log(err))
     })
@@ -79,11 +81,24 @@ io.on('connection', function (socket) {
 
     socket.on('start_send', (msg) => {
         sendpulse.init(API_USER_ID,API_SECRET,TOKEN_STORAGE,function() {
-            sendpulse.createAddressBook((res) => { //возвращает id книги // error { error_code: 203, message: 'Book name already in use' }
-                if(res.id){//если создали книгу контактов
-                    sendpulse.addEmails((res) => { //заполняем книгу адресами
-                        console.log(res)
-                    }, res.id, msg.emails)
+            sendpulse.createAddressBook((book) => { //возвращает id книги // error { error_code: 203, message: 'Book name already in use' }
+                console.log('book',book)
+                if(book.id){//если создали книгу контактов
+                    sendpulse.addEmails((added) => { //заполняем книгу адресами
+                        console.log('added', added)
+                        if(added.result){
+                            sendpulse.getBookInfo((bookInfo) => {
+                                console.log('bookInfo', bookInfo[0])
+                                if(bookInfo[0].status === 0){ // проверяем состояние книги, если 0, то можно рассылать
+                                    sendpulse.createCampaign((res) => {
+                                        console.log(res)
+                                    }, "ИМИТ ВолГУ", SENDER_EMAIL, msg.subject, msg.template,
+                                        book.id)
+                                }
+                            }, book.id)
+
+                        }
+                    }, book.id, msg.emails)
                 }
             }, hash(Date.now()))
         });
